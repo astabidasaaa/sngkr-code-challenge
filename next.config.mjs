de66b60cf1d041817904782c2e29619faee74ea7
astabidasaaa <astabidasabcd@gmail.com>
2024-06-15 04:42:24 +0700

    init: with create-next-app

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  images: {
+    formats: ["image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.ctfassets.net",
+      },
+    ],
+  },
+  env: {
+    CONTENTFUL_SPACE_ID: "3qwbgvc04ikj",
+    CONTENTFUL_ACCESS_TOKEN: "Nf8b6MNghhgerbGmawb5f0GIhJBUe4HwewI--Q4IDPI",
+    CONTENTFUL_PREVIEW_ACCESS_TOKEN:
+      "H_7WZ_7NRZ_Bgo6KJdd4xFpMuY8Owtz9BF6PD_LSYeM",
+  },
+};
 
 export default nextConfig;
