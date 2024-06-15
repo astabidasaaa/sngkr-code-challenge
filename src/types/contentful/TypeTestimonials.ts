347ccaee19dec91ad6556189d92a4706c9a42ee2
astabidasaaa <astabidasabcd@gmail.com>
2024-06-16 04:08:24 +0700

    fix: reference (product) types & fix typo

--- src/types/contentful/TypeTestimonials.ts
+++ src/types/contentful/TypeTestimonials.ts
@@ -1,15 +1,23 @@
-import type { Entry, EntryFields } from "contentful";
+import type { Asset, Entry, EntryFields } from "contentful";
 
 type T = {
   contentTypeId: string;
-  fields: Record<string, any>;
+  // fields: Record<string, any>;
+  fields: {
+    name: EntryFields.Symbol;
+    image: Asset;
+    shortDescription: EntryFields.Symbol;
+    longDescription: EntryFields.RichText;
+    price: EntryFields.Integer;
+    slug: EntryFields.Symbol;
+  };
 };
 
 export interface TypeTestimonialsFields {
   contentTypeId: "testimonials";
   fields: {
     name: EntryFields.Symbol;
-    testimoni: EntryFields.Symbol;
+    testimony: EntryFields.Symbol;
     product: Entry<T>;
   };
 }
