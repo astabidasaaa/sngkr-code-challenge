e1726b478b552e0212c41a769b02a414f0613b96
astabidasaaa <astabidasabcd@gmail.com>
2024-06-17 19:05:07 +0700

    feat: add tagline & social btn

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,15 +1,45 @@
 import Image from "next/image";
 import React from "react";
 import MainLogo from "./mainLogo";
+import { Instagram } from "lucide-react";
+import { FaInstagram, FaYoutube } from "react-icons/fa6";
+import { Button } from "./ui/button";
 
 const Footer = () => {
   return (
-    <div className="relative flex justify-center items-center min-h-96 w-full">
-      <div className="relative flex flex-col justify-center items-center z-10 ">
-        <MainLogo className="w-48 md:w-56 fill-primary" />
-        <span className="text-sm font-light text-primary font-lato tracking-widest">
+    <footer className="relative flex flex-col justify-center items-center w-full px-4 sm:px-10 md:px-16 lg:px-20">
+      <div className="relative flex flex-col md:flex-row justify-center md:justify-center items-center gap-10 min-h-96 w-full z-10">
+        <div className="relative flex flex-col justify-center items-center">
+          <MainLogo className="w-48 md:w-56 h-16 md:h-20 fill-primary" />
+          <span className="text-md text-center font-light text-primary font-lato tracking-widest">
+            Crafting Excellence, Inspiring Creativity
+          </span>
+        </div>
+      </div>
+      <div className="flex justify-between items-center w-full py-4 z-10">
+        <span className="text-center text-sm font-light text-primary font-lato tracking-wide">
           © 2024 Brushscape by sngkr
         </span>
+        <div className="flex flex-row gap-2">
+          <Button asChild variant="link" className="p-0">
+            <a
+              href="https://instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram className="size-6 text-primary/70" />
+            </a>
+          </Button>
+          <Button asChild variant="link" className="p-0">
+            <a
+              href="https://youtube.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaYoutube className="size-6 text-primary/70" />
+            </a>
+          </Button>
+        </div>
       </div>
       <Image
         src="/footer-bg.webp"
@@ -17,7 +47,7 @@ const Footer = () => {
         fill
         className="absolute top-0 opacity-60 brightness-110 object-cover object-center z-0"
       />
-    </div>
+    </footer>
   );
 };
 
