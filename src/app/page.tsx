da96f94294bf34ef93a229017a85a9d97ef98044
astabidasaaa <astabidasabcd@gmail.com>
2024-06-18 21:38:14 +0700

    feat: metadata title & description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import HomeView from "@/view/home";
-// import Image from "next/image";
+import type { Metadata } from "next";
 
-export default function Home() {
+export const metadata: Metadata = {
+  description:
+    "Discover Brushscape's exceptional range of paintbrushes designed for artists of all levels. Explore our collection and find the perfect tools to bring your creative visions to life.",
+};
+
+const HomePage = () => {
   return (
     <div className="relative flex flex-col items-start justify-start min-h-screen w-full">
       <HomeView />
     </div>
   );
-}
+};
+
+export default HomePage;
